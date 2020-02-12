cf2b5bfda6b01f0b289e2b1f8822216cb1f43e30
Nabeel Siddiqui <n.siddiqui25@gmail.com>
2020-02-12 11:42:21 -0600

    keep code DRY by using 'form-level' variables

--- Calculator/Calculator/Form1.vb
+++ Calculator/Calculator/Form1.vb
@@ -1,8 +1,11 @@
 ﻿Public Class frmCalculator
+    Dim firstNum, secondNum, result As Double
+
     Private Sub btnPlus_Click(sender As Object, e As EventArgs) Handles btnPlus.Click
-        Dim firstNum, secondNum, result As Double
+        'Dim firstNum, secondNum, result As Double
         'first we create some variables for inputs that will hold the first/second numbers and the result
         'Double because we can use decimal numbers, and not just integers 
+        'As we code the other events, we can see these local variables are being repeated for each event. In order to use them in all the click-events we declare them as "form-level" variables. So we can get rid of these in all three event operations and place them right after Public Class frmCalculator
 
 
         'first, we make sure the user entered something into the textboxes
@@ -22,7 +25,7 @@
     End Sub
 
     Private Sub btnMinus_Click(sender As Object, e As EventArgs) Handles btnMinus.Click
-        Dim firstNum, secondNum, result As Double
+        'Dim firstNum, secondNum, result As Double
         If txtFirst.Text <> "" And txtSecond.Text <> "" Then
             firstNum = CDbl(txtFirst.Text)
             secondNum = CDbl(txtSecond.Text)
@@ -33,7 +36,7 @@
     End Sub
 
     Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
-        Dim firstNum, secondNum, result As Double
+        'Dim firstNum, secondNum, result As Double
         If txtFirst.Text <> "" And txtSecond.Text <> "" Then
             firstNum = CDbl(txtFirst.Text)
             secondNum = CDbl(txtSecond.Text)
