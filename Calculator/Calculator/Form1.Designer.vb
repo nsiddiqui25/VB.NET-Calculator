8d91c03bf4a2a91bc8cc8ff9f2c5fa5ce27e8571
Nabeel Siddiqui <n.siddiqui25@gmail.com>
2020-02-12 10:17:47 -0600

    replace old project with new code

---
+++ Calculator/Calculator/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFirst = New System.Windows.Forms.TextBox()
+        Me.txtSecond = New System.Windows.Forms.TextBox()
+        Me.txtResults = New System.Windows.Forms.TextBox()
+        Me.btnPlus = New System.Windows.Forms.Button()
+        Me.btnMinus = New System.Windows.Forms.Button()
+        Me.btnMultiply = New System.Windows.Forms.Button()
+        Me.lblFirst = New System.Windows.Forms.Label()
+        Me.lblSecond = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtFirst
+        '
+        Me.txtFirst.Location = New System.Drawing.Point(36, 131)
+        Me.txtFirst.Name = "txtFirst"
+        Me.txtFirst.Size = New System.Drawing.Size(200, 20)
+        Me.txtFirst.TabIndex = 0
+        '
+        'txtSecond
+        '
+        Me.txtSecond.Location = New System.Drawing.Point(468, 131)
+        Me.txtSecond.Name = "txtSecond"
+        Me.txtSecond.Size = New System.Drawing.Size(200, 20)
+        Me.txtSecond.TabIndex = 1
+        '
+        'txtResults
+        '
+        Me.txtResults.Location = New System.Drawing.Point(36, 265)
+        Me.txtResults.Name = "txtResults"
+        Me.txtResults.ReadOnly = True
+        Me.txtResults.Size = New System.Drawing.Size(632, 20)
+        Me.txtResults.TabIndex = 2
+        '
+        'btnPlus
+        '
+        Me.btnPlus.Location = New System.Drawing.Point(313, 22)
+        Me.btnPlus.Name = "btnPlus"
+        Me.btnPlus.Size = New System.Drawing.Size(75, 75)
+        Me.btnPlus.TabIndex = 3
+        Me.btnPlus.Text = "+"
+        Me.btnPlus.UseVisualStyleBackColor = True
+        '
+        'btnMinus
+        '
+        Me.btnMinus.Location = New System.Drawing.Point(313, 103)
+        Me.btnMinus.Name = "btnMinus"
+        Me.btnMinus.Size = New System.Drawing.Size(75, 75)
+        Me.btnMinus.TabIndex = 4
+        Me.btnMinus.Text = "-"
+        Me.btnMinus.UseVisualStyleBackColor = True
+        '
+        'btnMultiply
+        '
+        Me.btnMultiply.Location = New System.Drawing.Point(313, 184)
+        Me.btnMultiply.Name = "btnMultiply"
+        Me.btnMultiply.Size = New System.Drawing.Size(75, 75)
+        Me.btnMultiply.TabIndex = 5
+        Me.btnMultiply.Text = "x"
+        Me.btnMultiply.UseVisualStyleBackColor = True
+        '
+        'lblFirst
+        '
+        Me.lblFirst.AutoSize = True
+        Me.lblFirst.Location = New System.Drawing.Point(91, 115)
+        Me.lblFirst.Name = "lblFirst"
+        Me.lblFirst.Size = New System.Drawing.Size(66, 13)
+        Me.lblFirst.TabIndex = 6
+        Me.lblFirst.Text = "First Number"
+        '
+        'lblSecond
+        '
+        Me.lblSecond.AutoSize = True
+        Me.lblSecond.Location = New System.Drawing.Point(525, 115)
+        Me.lblSecond.Name = "lblSecond"
+        Me.lblSecond.Size = New System.Drawing.Size(84, 13)
+        Me.lblSecond.TabIndex = 7
+        Me.lblSecond.Text = "Second Number"
+        '
+        'frmCalculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(721, 343)
+        Me.Controls.Add(Me.lblSecond)
+        Me.Controls.Add(Me.lblFirst)
+        Me.Controls.Add(Me.btnMultiply)
+        Me.Controls.Add(Me.btnMinus)
+        Me.Controls.Add(Me.btnPlus)
+        Me.Controls.Add(Me.txtResults)
+        Me.Controls.Add(Me.txtSecond)
+        Me.Controls.Add(Me.txtFirst)
+        Me.Name = "frmCalculator"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFirst As TextBox
+    Friend WithEvents txtSecond As TextBox
+    Friend WithEvents txtResults As TextBox
+    Friend WithEvents btnPlus As Button
+    Friend WithEvents btnMinus As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents lblFirst As Label
+    Friend WithEvents lblSecond As Label
+End Class
